9eb0f5b1f5fc4a4ed0756800555ab4c887ea2fc0
Kennedy North Ford <62063125+kennedyford@users.noreply.github.com>
2021-11-08 22:44:57 -0600

    Add files via upload

--- p1/p1.js
+++ p1/p1.js
@@ -2,7 +2,7 @@ var tokens = localStorage.getItem("tokens")
 var tokenDisplay = document.getElementById("tokens")
 
 if (!tokens) {
-	localStorage.setItem("tokens", 20)
+	localStorage.setItem("tokens", 100)
 	tokenDisplay.innerHTML += localStorage.getItem("tokens")
 } else if (tokens <= 0) {
 	tokenDisplay.innerHTML += "None.<br>Go to one<br>of the games to<br>redeem more<br>tokens."
